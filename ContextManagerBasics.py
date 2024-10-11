154af5b353529017e429efdf6c7816df0ae3f668
KalebYigezu <43226925+KalebYigezu@users.noreply.github.com>
2024-10-11 12:57:37 -0700

    Update ContextManagerBasics.py

--- ContextManagerBasics.py
+++ ContextManagerBasics.py
@@ -17,3 +17,23 @@ with FileManager('testt.txt', 'w') as f:
     f.write('Test')
 
 print(f.closed)
+
+------------------------------------------------------------------
+
+class File:
+    def __init__(self, name: str):
+        self.name = name
+
+    def __enter__(self):
+        print(f"Opening {self.name}...")
+
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(f"Closing {self.name}...")
+
+
+with File("Main.py") as file:
+    print(file.name)
+
+print("Done")
